4c8370c7a2787617e4616d79f35ad548161c2688
Angel Aranguiz <angel.aranguiz@gmail.com>
2022-08-21 05:04:57 -0400

    Added Dotenv and tested DB connection successfully

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,30 @@
 <?php 
 class Database {
-  private $db_host = "localhost";
-  private $db_name = "mydb";
-  private $db_user = "root";
-  private $db_password = "";
+  private $db_host;
+  private $db_name;
+  private $db_user;
+  private $db_password;
   private $connection;
+
+  public function init() {
+    $this->db_host = $_ENV['DB_HOST'];
+    $this->db_name = $_ENV["DB_NAME"];
+    $this->db_user = $_ENV["DB_USER"];
+    $this->db_password = $_ENV["DB_PASSWORD"];
+  }
+
+  public function getConnection() {
+    $this->connection = null;
+
+    try {
+        $this->connection = new PDO("mysql:host=" . $this->db_host . ";dbname=" . $this->db_name, $this->db_user, $this->db_password);
+        echo "Connection successful!";
+    } catch(PDOException $exception){
+        echo "Connection failed: " . $exception->getMessage();
+    }
+
+    return $this->connection; 
+  }
+
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-  echo 'Hello world';
+  require "vendor/autoload.php";
+  include_once './config/database.php';
+
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->safeLoad();
+
+  $database = new Database();
+  $database->init();
+  $database->getConnection();
+
+  
 ?>
